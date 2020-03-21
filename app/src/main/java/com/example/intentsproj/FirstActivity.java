0fdbf71838d265974804a17dce1243b624a4c8b7
Shalitha <shalithax@gmail.com>
2020-03-21 16:26:30 +0530

    Implemented custom toast

--- app/src/main/java/com/example/intentsproj/FirstActivity.java
+++ app/src/main/java/com/example/intentsproj/FirstActivity.java
@@ -6,7 +6,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -23,6 +25,19 @@ public class FirstActivity extends AppCompatActivity {
         btnOK = findViewById(R.id.btnOK);
         txtNumber1 = findViewById(R.id.txtNumber1);
         txtNumber2 = findViewById(R.id.txtNumber2);
+
+        //Creating the LayoutInflater instance
+        LayoutInflater li = getLayoutInflater();
+        //Getting the View object as defined in the customtoast.xml file
+        View layout = li.inflate(R.layout.custom_toast, (ViewGroup)
+                findViewById(R.id.custom_toast_layout));
+
+        //Creating the Toast object
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setView(layout);//setting the view of custom toast layout
+        toast.show();
 
         btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
